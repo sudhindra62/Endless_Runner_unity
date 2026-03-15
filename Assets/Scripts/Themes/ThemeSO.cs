f4c3a3dbc84c1c443be7a56e6def673422e7dbb5
kumarsudhindra2910 <kumarsudhindra2910@gmail.com>
2026-03-15 15:50:36 +0000

    Changes before Firebase Studio auto-run

--- Assets/Scripts/Themes/ThemeSO.cs
+++ Assets/Scripts/Themes/ThemeSO.cs
@@ -3,12 +3,44 @@ using UnityEngine;
 
 namespace EndlessRunner.Themes
 {
+    public enum ThemeUnlockType
+    {
+        Free,
+        Gems,
+        Premium
+    }
+
     [CreateAssetMenu(fileName = "ThemeSO", menuName = "EndlessRunner/ThemeSO", order = 0)]
     public class ThemeSO : ScriptableObject
     {
+        [Header("Theme Info")]
         public string themeName;
+        public GameObject themePreviewPrefab;
+
+        [Header("Unlock Conditions")]
+        public ThemeUnlockType unlockType;
+        public int gemCost;
+
+        [Header("Visuals")]
+        public Material skybox;
+        public Color lightingColor = Color.white;
+        public float lightingIntensity = 1.0f;
+        public Color fogColor = Color.grey;
+        public float fogDensity = 0.01f;
+
+        [Header("Game Element Prefabs")]
+        public GameObject[] environmentModules;
+        public GameObject coinPrefab;
+        public GameObject enemyChaserPrefab;
+
+        [Header("Materials")]
         public Material groundMaterial;
         public Material obstacleMaterial;
-        public Color skyColor;
+
+        [Header("Audio")]
+        public AudioClip backgroundMusic;
+        
+        [Header("UI")]
+        public Color uiAccentColor = Color.white;
     }
 }
