f4ba5136ba692c2cae1f5ec95bdb5b7445a14fd6
Sudhindra Kumar <kumarsudhindra2910@gmail.com>
2026-03-23 23:59:00 +0530

    after scoremanager.cs improvisions

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
     public class ScoreManager : Singleton<ScoreManager>
     {
-            public static event Action<int> OnScoreChanged;
-            public static event Action<int> OnHighScoreChanged;
-            public static event Action<int> OnScoreUpdated; // Alias for OnScoreChanged
-            public static event Action<int> OnCoinsUpdated;
-            public static event Action<long> OnScoreMilestone;
-        public int Score { get; private set; }
+        public static event Action<int> OnScoreChanged;
+        public static event Action<int> OnHighScoreChanged;
+        public static event Action<int> OnScoreUpdated; // Alias for OnScoreChanged
+        public static event Action<long> OnScoreMilestone;
         public int HighScore { get; private set; }
         public int BestScore => HighScore; // Alias
         
         public float CoinMultiplier { get; set; } = 1f;
         private float scoreMultiplier = 1f;
         private float multiplierCap = 10f;
-
         private const string HighScoreKey = "HighScore";
 
         protected override void Awake()
@@ -26,18 +23,15 @@ using UnityEngine;
             DontDestroyOnLoad(gameObject);
             LoadHighScore();
         }
-
         private void OnEnable()
         {
             GameManager.OnGameStateChanged += OnGameStateChanged;
         }
-
         private void OnDisable()
         {
             if (GameManager.Instance != null)
                 GameManager.OnGameStateChanged -= OnGameStateChanged;
         }
-
         private void OnGameStateChanged(GameState newState)
         {
             if (newState == GameState.Playing)
@@ -48,15 +42,9 @@ using UnityEngine;
 
         public void AddScore(int amount)
         {
-            int previousMilestoneBucket = Score / 1000;
             Score += amount;
             OnScoreChanged?.Invoke(Score);
             OnScoreUpdated?.Invoke(Score);
-            int currentMilestoneBucket = Score / 1000;
-            if (currentMilestoneBucket > previousMilestoneBucket)
-            {
-                OnScoreMilestone?.Invoke(currentMilestoneBucket * 1000L);
-            }
 
             if (Score > HighScore)
             {
@@ -115,19 +103,5 @@ using UnityEngine;
         private void SaveHighScore()
         {
             SaveManager.Instance.SaveGame();
-        }
-
-        private void LoadHighScore()
-        {
-            Score = 0;
-            if (SaveManager.Instance != null)
-            {
-                HighScore = SaveManager.Instance.Data.highScore;
-            }
-        }
-
-        public void UpdateCoins(int coins)
-        {
-            OnCoinsUpdated?.Invoke(coins);
         }
     }
